d21eee708e6cb805e3f8eabff1078b0d45b35859
Kolbeev Grigory <122559765+TOKYOGATSBY@users.noreply.github.com>
2023-03-01 09:42:34 +0300

    Add files via upload

---
+++ Homework 09.02.2023/main.cpp
@@ -0,0 +1,25 @@
+//Задание 1. Дана строка символов. Заменить в ней все пробелы на табуляции.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string s1;
+
+    cout << "Enter the string\n";
+    getline (cin, s1);
+
+
+    for (int i = 0; i < s1.size(); ++i) {
+        if (s1 [i] == ' ') {
+            s1 [i] = '\t';
+        }
+    }
+
+    cout << s1 << endl;
+
+    return 0;
+}
